72af67df8db2b415773740d3d8a2cf2af45f1279
donaf <2420639180@qq.com>
2022-07-26 07:57:04 +0800

    feat: 抽离TodoItem组件

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,114 @@
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+interface Todo {
+  status: boolean;
+  text: string;
+  editable: boolean; //是否可编辑
+}
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+/**
+ * 删除
+ */
+const onDelete = () => {};
+
+/**
+ * 编辑
+ * @param item
+ */
+const onClickInput = () => {
+};
+
+/**
+ * 确认修改
+ * @param item
+ */
+const onConfirm = () => {};
+
+/**
+ * input失焦
+ * @param item
+ * @param index
+ * @param e
+ */
+const onBlurEdit = () => {};
+
+</script>
+
+<template>
+  <li class="li">
+    <span class="li-left">
+      <input type="checkbox" class="checkbox" v-model="props.todo.status" />
+      <b>
+        <input
+          class="li-text"
+          :class="{ 'li-text_done': props.todo.status }"
+          type="text"
+          v-model="props.todo.text"
+          @click="onClickInput"
+          @blur="onBlurEdit"
+        />
+        <button
+          class="btn-confirm"
+          @click="onConfirm"
+          v-if="props.todo.editable"
+        >
+          确认
+        </button>
+      </b>
+    </span>
+    <span class="delete" @click="onDelete">删除</span>
+  </li>
+</template>
+
+<style scoped>
+.li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 16px;
+  padding-bottom: 16px;
+  font-size: 16px;
+  border-bottom: 1px solid #ddd;
+}
+.li-left {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.checkbox {
+  margin-right: 10px;
+  width: 24px;
+  height: 24px;
+}
+.li-text {
+  padding: 8px 10px;
+  font-size: 16px;
+  background: none;
+  border: none;
+}
+.li-text_done {
+  text-decoration: line-through;
+}
+
+.li-text_active {
+  border: 1px solid #ddd;
+  border-radius: 3px;
+}
+.delete {
+  color: red;
+  cursor: pointer;
+}
+
+.btn-confirm {
+  margin-left: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { ref, reactive, computed } from "vue";
+import TodoItem from "../components/TodoItem.vue";
 
+// TODO interface抽离
 interface Todo {
   status: boolean;
   text: string;
@@ -46,47 +48,8 @@ const add = () => {
   text.value = "";
 };
 
-/**
- * 删除
- */
-const onDelete = (index: number) => {
-  list.splice(index, 1);
-};
-
 const leftNum = computed(() => list.filter((item) => !item.status).length);
 
-/**
- * 编辑
- * @param item
- */
-const onClickInput = (item: Todo, e: any) => {
-  item.editable = true;
-};
-
-/**
- * 确认修改
- * @param item
- */
-const onConfirm = (item: Todo, index: number) => {
-  // list[index].editable = true;
-  if (!item.text.trim()) {
-    list.splice(index, 1);
-  }
-  item.editable = false;
-};
-
-/**
- * input失焦
- * @param item
- * @param index
- * @param e
- */
-const onBlurEdit = (item: Todo, index: number, e: any) => {
-  if (!e.target.value.trim()) {
-    list.splice(index, 1);
-  }
-  item.editable = false;
-};
 
 /**
  * 全选
@@ -118,30 +81,7 @@ const toggleChecked = () => {
     <div class="content" v-if="list && list.length > 0">
       <h1 class="title">列表</h1>
       <ul>
-        <li class="li" v-for="(item, index) in list" :key="index">
-          <span class="li-left">
-            <input type="checkbox" class="checkbox" v-model="item.status" />
-            <b>
-              <input
-                :ref="`liText[${index}]`"
-                class="li-text"
-                :class="{ 'li-text_done': item.status }"
-                type="text"
-                v-model="item.text"
-                @click="onClickInput(item, $event)"
-                @blur="onBlurEdit(item, index, $event)"
-              />
-              <button
-                class="btn-confirm"
-                @click="onConfirm(item, index)"
-                v-if="item.editable"
-              >
-                确认
-              </button>
-            </b>
-          </span>
-          <span class="delete" @click="onDelete(index)">删除</span>
-        </li>
+        <TodoItem v-for="(todo, index) in list" :key="index" :todo="todo" />
       </ul>
     </div>
     <div class="footer" v-if="list && list.length > 0">
@@ -198,48 +138,6 @@ const toggleChecked = () => {
 ul,
 li {
   list-style: none;
-}
-.li {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  margin-bottom: 16px;
-  padding-bottom: 16px;
-  font-size: 16px;
-  border-bottom: 1px solid #ddd;
-}
-.li-left {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.checkbox {
-  margin-right: 10px;
-  width: 24px;
-  height: 24px;
-}
-.li-text {
-  padding: 8px 10px;
-  font-size: 16px;
-  background: none;
-  border: none;
-}
-.li-text_done {
-  text-decoration: line-through;
-}
-
-.li-text_active {
-  border: 1px solid #ddd;
-  border-radius: 3px;
-}
-.delete {
-  color: red;
-  cursor: pointer;
-}
-
-.btn-confirm {
-  margin-left: 10px;
 }
 .allChecked-container {
   display: flex;
